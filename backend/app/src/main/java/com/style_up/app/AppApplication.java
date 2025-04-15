9005f40a5ff69a0f2ec22cbf1f562a81764447e7
AlSharaiyra <walidkhsharaiyra@gmail.com>
2025-04-15 22:33:52 +0300

    Setting up Spring Boot backend project

---
+++ backend/app/src/main/java/com/style_up/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.style_up.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
