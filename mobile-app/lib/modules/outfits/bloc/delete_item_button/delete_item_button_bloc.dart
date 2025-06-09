28905eb69bdfdd761c04ed34dc4090c37c12a19e
Ayman Adas <aymanhaniadas28@gmail.com>
2025-06-09 17:45:46 +0300

    add remove bloc

---
+++ mobile-app/lib/modules/outfits/bloc/delete_item_button/delete_item_button_bloc.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:style_up/core/config/secure_token_storage.dart';
+import 'package:style_up/core/constant/error_codes.dart';
+import 'package:style_up/modules/outfits/controller/outfit_controller.dart';
+import 'package:style_up/modules/outfits/model/upload_warrdrop_item.dart';
+import 'package:style_up/modules/outfits/params/upload_outfit.dart';
+import 'delete_item_button_event.dart';
+import 'delete_item_button_state.dart';
+
+class DeleteItemButtonBloc extends Bloc<DeleteItemButtonEvent, DeleteItemButtonState> {
+  DeleteItemButtonBloc() : super(OnInit()) {
+    on<DeleteItemButtonPressed>(_onUploadButtonPressed);
+  }
+
+  Future<void> _onUploadButtonPressed(
+    DeleteItemButtonPressed event,
+    Emitter<DeleteItemButtonState> emit,
+  ) async {
+    emit(OnLoading());
+
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      final SecureTokenStorage secureTokenStorage = SecureTokenStorage.instance;
+      final response = await OutfitController()
+          .addOutfit(UploadOutfitParams(image: event.file, description: event.desc,accessToken: await secureTokenStorage.getAccessToken()??''), event.context);
+      response.fold(
+        (String l) {
+          final errorMessage = getErrorMessage(event.context, l);
+
+          emit(OnFailed(errorMessage: errorMessage));
+        },
+        (UploadWarddropItem r) {
+
+
+          emit(OnSuccess());
+        },
+      );
+    } catch (e) {
+      emit(OnFailed(errorMessage: e.toString()));
+    }
+  }
+}
